0fca550344575a0dc9c48c72487b9e9b2a51f5eb
RatKinng <mjfronek@alaska.edu>
2026-04-23 11:38:19 -0800

    Readjusted file locations, begun tutorial

---
+++ Tutorial/tutorial.py
@@ -0,0 +1,8 @@
+# libraries for dataset preparation, feature engineering, model training 
+
+from sklearn import model_selection, preprocessing, linear_model, naive_bayes, metrics, svm
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
+from sklearn import decomposition, ensemble
+
+import pandas, xgboost, numpy, textblob, string
+
